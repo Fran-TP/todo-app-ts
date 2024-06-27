5c3a1240e17f4f96dabc3be04d4caf3b84c84bb3
Fran-TP <ftorresp@unasam.edu.pe>
2024-06-27 16:15:11 -0500

    Add feature X to the todo app
    This commit adds feature X to the todo app, which allows users to perform a specific action. The implementation includes changes to the frontend and backend components, ensuring seamless integration and functionality. Additionally, relevant tests have been added to ensure the feature works as expected.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,14 @@ interface Props {
 const Header: React.FC<Props> = ({ addTodo }) => {
   return (
     <header>
-      <h1 className='text-[#b83f45] text-center text-7xl my-5'>todos</h1>
+      <h1 className='text-[#b83f45] text-center text-7xl my-5'>
+        todos
+        <img
+          aria-label='typescript logo'
+          className='w-12 h-12 inline-block ml-2 mb-2'
+          src='https://svgl.app/library/typescript.svg'
+        />
+      </h1>
       <CreateTodo addTodo={addTodo} />
     </header>
   )
