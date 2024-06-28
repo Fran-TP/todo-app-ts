22138597b0425f2d6004b69dca9397109780d328
Fran-TP <ftorresp@unasam.edu.pe>
2024-06-28 00:01:38 -0500

    feat: new prop interface onEditTodo created

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,22 +1,27 @@
-// cSpell: words todos
-import { type TodoId, type ListOfTodos } from '../types'
+// cSpell: words todos formkit
+import { type TodoId, type ListOfTodos, type Todo as TodoType } from '../types'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 import Todo from './Todo'
 
 interface Props {
   todos: ListOfTodos
+  onEditTodo: ({ id, text }: Omit<TodoType, 'completed'>) => void
   onRemoveTodo: ({ id }: TodoId) => void
   onToggleTodo: ({ id }: TodoId) => void
 }
 
-const Todos: React.FC<Props> = ({ todos, onRemoveTodo, onToggleTodo }) => {
+const Todos: React.FC<Props> = ({ todos, onEditTodo, onRemoveTodo, onToggleTodo }) => {
+  const [parent] = useAutoAnimate()
+
   return (
-    <ul className='list-none w-80 sm:w-[550px] divide-y divide-[#e6e6e6] border-x border-[#e6e6e6]'>
+    <ul ref={parent} className='list-none w-80 sm:w-[550px] divide-y divide-[#e6e6e6] border-x border-[#e6e6e6]'>
       {todos.map(todo => (
         <li key={todo.id}>
           <Todo
             id={todo.id}
             text={todo.text}
             completed={todo.completed}
+            onEditTodo={onEditTodo}
             onRemoveTodo={onRemoveTodo}
             onToggleTodo={onToggleTodo}
           />
