4794ebdae62b08797f8c86728ef096c22d12a709
Fran-TP <ftorresp@unasam.edu.pe>
2024-06-28 00:00:51 -0500

    feat: new prop interface onEditTodo created

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,18 +1,26 @@
+import { useEffect, useRef, useState } from 'react'
 import { type TodoId, type Todo as TodoTypes } from '../types'
 
 interface Props extends TodoTypes {
   onRemoveTodo: ({ id }: TodoId) => void
   onToggleTodo: ({ id }: TodoId) => void
+  onEditTodo: ({ id, text }: Omit<TodoTypes, 'completed'>) => void
 }
 
 const Todo: React.FC<Props> = ({
   id,
   text,
   completed,
+  onEditTodo,
   onRemoveTodo,
   onToggleTodo
 }) => {
-  const handleChangeCheckbox = (_event: React.ChangeEvent<HTMLInputElement>): void => {
+  const [editing, setEditing] = useState(false)
+  const [editedText, setEditedText] = useState(text)
+
+  const inputEditRef = useRef<HTMLInputElement>(null)
+
+  const handleChangeCheckbox = (): void => {
     onToggleTodo({ id })
   }
 
@@ -20,34 +28,86 @@ const Todo: React.FC<Props> = ({
     onRemoveTodo({ id })
   }
 
+  const handleChangeText = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setEditedText(e.target.value)
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === 'Enter') {
+      setEditedText(editedText.trim())
+
+      if (editedText.trim() === '') {
+        onRemoveTodo({ id })
+      }
+
+      if (editedText.trim() !== text) {
+        onEditTodo({ id, text: editedText.trim() })
+        setEditing(false)
+      }
+    }
+
+    if (e.key === 'Escape') {
+      setEditing(false)
+    }
+  }
+
+  const handleDoubleClick = (): void => {
+    setEditing(true)
+  }
+
+  const handleBlur = (): void => {
+    setEditing(false)
+    setEditedText(text)
+  }
+
+  useEffect(() => {
+    inputEditRef.current?.focus()
+  }, [editing])
+
   return (
-    <div
-      className='flex items-center gap-2 h-[60px] px-2 group'
-      data-todo-id={id}
-    >
-      <input
-        name='completed'
-        type='checkbox'
-        aria-label='completed'
-        checked={completed}
-        onChange={handleChangeCheckbox}
-        className='peer h-8 w-8 cursor-pointer border-gray-400 focus:ring-transparent rounded-full border checked:text-transparent checked:border-green-600 checked:hover:border-green-600 checked:hover:bg-transparent checked:focus:border-green-600 checked:focus:bg-transparent'
-      />
-      <label
-        htmlFor='completed'
-        className='peer-checked:line-through peer-checked:text-gray-500 transition-colors duration-300 ease-in-out'
+    <>
+      <div
+        className={`flex items-center gap-2 h-[60px] px-2 group ${editing ? 'hidden' : ''}`}
+        data-todo-id={id}
+        onDoubleClick={handleDoubleClick}
       >
-        {text}
-      </label>
-      <button
-        className='ml-auto opacity-0 group-hover:opacity-100 transition-opacity duration-150 ease-in-out'
-        onClick={handleClickRemove}
-      >
-        <span role='img' aria-label='delete'>
-          ❌
-        </span>
-      </button>
-    </div>
+        <input
+          id={id}
+          name='completed'
+          type='checkbox'
+          aria-label='completed'
+          checked={completed}
+          onChange={handleChangeCheckbox}
+          className='peer h-8 w-8 cursor-pointer border-gray-400 focus:ring-transparent rounded-full border checked:text-transparent checked:border-green-600 checked:hover:border-green-600 checked:hover:bg-transparent checked:focus:border-green-600 checked:focus:bg-transparent'
+        />
+        <label
+          htmlFor={id}
+          className='peer-checked:line-through peer-checked:text-gray-500 transition-colors duration-300 ease-in-out'
+        >
+          {text}
+        </label>
+        <button
+          className='ml-auto opacity-0 group-hover:opacity-100 transition-opacity duration-150 ease-in-out'
+          onClick={handleClickRemove}
+        >
+          <span role='img' aria-label='delete'>
+            ❌
+          </span>
+        </button>
+      </div>
+      {editing && (
+        <input
+          className='w-full h-[60px] px-2 py-1 border-gray-400 focus:border-[#b83f45] border-2 focus:ring-0'
+          name='text'
+          type='text'
+          value={editedText}
+          onChange={handleChangeText}
+          onBlur={handleBlur}
+          onKeyDown={handleKeyDown}
+          ref={inputEditRef}
+        />
+      )}
+    </>
   )
 }
 
