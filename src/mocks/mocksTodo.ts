d344ccfcb3a0a535cd2def79071ce64e3ca8559a
Fran-TP <ftorresp@unasam.edu.pe>
2024-07-02 17:07:28 -0500

    feat: Add mocksTodo file with mock data for todos

---
+++ src/mocks/mocksTodo.ts
@@ -0,0 +1,8 @@
+// cSpell:ignore mockTodos
+
+export const mockTodos = [
+  { id: '1', text: 'Learn React', completed: true },
+  { id: '2', text: 'Learn TypeScript', completed: true },
+  { id: '3', text: 'Learn Tailwind CSS', completed: false },
+  { id: '4', text: 'Learn GraphQL', completed: false }
+]
