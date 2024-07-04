bac5abe098c8ed3a13657a0ef9f0c5aa5a6882d0
Fran-TP <ftorresp@unasam.edu.pe>
2024-07-04 18:02:59 -0500

    refactor: Update Todos component to include ToggleAllButton and handle toggling all todo items

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,33 +1,58 @@
 // cSpell: words todos formkit
-import { type TodoId, type ListOfTodos, type Todo as TodoType } from '../types'
+import {
+  type TodoId,
+  type ListOfTodos,
+  type Todo as TodoType
+} from '../types'
 import { useAutoAnimate } from '@formkit/auto-animate/react'
 import Todo from './Todo'
+import ToggleAllTodos from './ToggleAllButton'
 
 interface Props {
+  isAllCompleted: boolean
   todos: ListOfTodos
   onEditTodo: ({ id, text }: Omit<TodoType, 'completed'>) => void
   onRemoveTodo: ({ id }: TodoId) => void
   onToggleTodo: ({ id }: TodoId) => void
+  onToggleAll: () => void
 }
 
-const Todos: React.FC<Props> = ({ todos, onEditTodo, onRemoveTodo, onToggleTodo }) => {
+const Todos: React.FC<Props> = ({
+  isAllCompleted,
+  todos,
+  onEditTodo,
+  onRemoveTodo,
+  onToggleTodo,
+  onToggleAll
+}) => {
   const [parent] = useAutoAnimate()
 
   return (
-    <ul ref={parent} className='list-none w-80 sm:w-[550px] divide-y divide-[#e6e6e6] border-x border-[#e6e6e6]'>
-      {todos.map(todo => (
-        <li key={todo.id}>
-          <Todo
-            id={todo.id}
-            text={todo.text}
-            completed={todo.completed}
-            onEditTodo={onEditTodo}
-            onRemoveTodo={onRemoveTodo}
-            onToggleTodo={onToggleTodo}
-          />
-        </li>
-      ))}
-    </ul>
+    <main className='relative'>
+      {todos.length > 0 && (
+        <ToggleAllTodos
+          isAllCompleted={isAllCompleted}
+          onToggleAll={onToggleAll}
+        />
+      )}
+      <ul
+        ref={parent}
+        className='list-none w-80 sm:w-[550px] divide-y divide-[#e6e6e6] border-x border-[#e6e6e6]'
+      >
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <Todo
+              id={todo.id}
+              text={todo.text}
+              completed={todo.completed}
+              onEditTodo={onEditTodo}
+              onRemoveTodo={onRemoveTodo}
+              onToggleTodo={onToggleTodo}
+            />
+          </li>
+        ))}
+      </ul>
+    </main>
   )
 }
 
