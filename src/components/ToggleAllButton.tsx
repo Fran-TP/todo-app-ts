5170177659c12538a83107d65ae97f5950a5cdea
Fran-TP <ftorresp@unasam.edu.pe>
2024-07-04 17:59:34 -0500

    feat: Add ToggleAllButton component for toggling all todo items

---
+++ src/components/ToggleAllButton.tsx
@@ -0,0 +1,29 @@
+// cSpell: words todos
+import { type TodoCompleted } from '../types'
+
+interface Props {
+  isAllCompleted: boolean
+  onToggleAll: ({ completed }: TodoCompleted) => void
+}
+
+const ToggleAllButton: React.FC<Props> = ({ isAllCompleted, onToggleAll }) => {
+  const handleToggleAllTodoItems = (): void => {
+    onToggleAll({ completed: true })
+  }
+
+  return (
+    <div className='absolute top-[-45px] left-2'>
+      <label htmlFor='toggleAllButton'>
+        <input
+          id='toggleAllButton'
+          className='w-8 h-8 focus:ring-transparent rounded-full text-transparent border-gray-400 checked:focus:border-green-600 checked:border-green-600 checked:hover:border-green-600'
+          type='checkbox'
+          checked={isAllCompleted}
+          onChange={handleToggleAllTodoItems}
+        />
+      </label>
+    </div>
+  )
+}
+
+export default ToggleAllButton
